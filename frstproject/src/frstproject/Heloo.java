4b2d5c37969d7c914dbdc317b1eb170ce76e8471
Admin <Admin@Admin-PC>
2017-02-27 14:50:36 +0530

    Initial checkin for 1st time is done.

---
+++ frstproject/src/frstproject/Heloo.java
@@ -0,0 +1,11 @@
+package frstproject;
+
+public class Heloo {
+
+	public static void main(String[] args) {
+		System.out.println("This in Running in sloar Qube");
+		System.out.println("my 2nd check in..:");
+
+	}
+
+}
