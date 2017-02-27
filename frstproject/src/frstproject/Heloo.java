f443051b55e837d5fd3b0f819d3ec88839e0383d
Admin <Admin@Admin-PC>
2017-02-27 15:40:10 +0530

    This Is My Last Commit.

--- frstproject/src/frstproject/Heloo.java
+++ frstproject/src/frstproject/Heloo.java
@@ -5,6 +5,7 @@ public class Heloo {
 	public static void main(String[] args) {
 		System.out.println("This in Running in sloar Qube");
 		System.out.println("my 2nd check in..:");
+		System.out.println("My Last Check into Local Repo:");
 
 	}
 
